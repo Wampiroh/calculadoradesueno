82b5be5d04bf48685c5228a96250b258b02b1fbe
wampiroh <wampiroh@gmail.com>
2022-06-16 13:52:30 -0500

    gatorandom

--- javascript.js
+++ javascript.js
@@ -76,7 +76,31 @@ function reset() {
 }
 
 function done() {
-  document.getElementById("swal").addEventListener('click', function(){
-    Swal.fire('Listo')
-  }
-)}
+  document.getElementById("swal").addEventListener("click", function () {
+    Swal.fire("Listo");
+  });
+}
+// fetch gato random de le good luck
+
+let generate_btn = document.querySelector(".generate_btn");
+
+generate_btn.addEventListener("click", fetchPics);
+
+function fetchPics() {
+  let catImgDiv = document.querySelector(".catImgDiv");
+  catImgDiv.innerHTML = "";
+
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      let catImgUrl = data[0].url;
+
+      let catImgEl = document.createElement("img");
+      catImgEl.setAttribute("src", `${catImgUrl}`);
+      catImgEl.classList.add("showcase");
+
+      let catImgDiv = document.querySelector(".catImgDiv");
+      catImgDiv.appendChild(catImgEl);
+    })
+    .catch((err) => console.log(err));
+}
